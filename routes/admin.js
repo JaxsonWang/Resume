9f4f9e24c2af68762f92447cf50796577efffd3b
JaxsonWang <wangjin@boxrom.com>
2017-05-23 16:35:07 +0800

    添加后台支持数据更新的接口

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,21 @@ var pool = mysql.createPool(config.mysql);
 router.get('/', function (req, res, next) {
     var sess = req.session;
     res.render('admin', {
-        title: '后台管理'
+        title: '后台管理',
+        name: sess.name
+    });
+});
+router.post('/admin/example', function (req, res, next) {
+    var example_1 = req.body.example_1;
+    var sess = req.session;
+    pool.getConnection(function (err, connection) {
+        var updateSql = "";
+        connection.query(updateSql, "Jaxson", function (err, result) {
+            var resultJson = result;
+            console.log(resultJson);
+            res.json(resultJson);
+            connection.release();
+        });
     });
 });
 
