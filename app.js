599d1b9c480127756f76f856449a5052029d0f15
JaxsonWang <wangjin@boxrom.com>
2017-05-24 11:29:13 +0800

    更新注释说明

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ var app = express();
 
 var identityKey = 'iiong';
 
-// view engine setup
+//pug --> 模板引擎设置
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
+// 公开网站图标
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -48,20 +48,20 @@ app.use('/register', register);
 app.use('/logout',logout);
 app.use('/admin',admin);
 
-// catch 404 and forward to error handler
+// 捕获异常，并且转发404错误
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handler
+// 处理错位程序
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
+    // 仅在开发显示相关错误
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
+    // 渲染错误页面
     res.status(err.status || 500);
     res.render('error');
 });
