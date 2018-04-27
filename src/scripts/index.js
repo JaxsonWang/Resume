fbfa6e3027a9cc5af6c83aa5b38545e16faef8e7
JaxsonWang <wangjin@boxrom.com>
2018-04-27 13:21:56 +0800

    更新版本

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+(function ($) {
+    $(document).ready(function(){
+        var contentHeight = $('#main-panel').height();
+        $('#side-nav').height(contentHeight);
 
-});
+        $(window).resize(function () {
+            var width = $(window).width();
+            if (width < 768) {
+                $('#side-nav').height('auto');
+            }
+        })
+    });
+})(jQuery);
